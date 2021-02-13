bf878a20576b7d9387d4509e04a9abf486f30909
vpcvdc <vpcvdc@hotmail.com>
2021-02-13 06:13:20 +0530

    update dependencies

--- src/components/Img.js
+++ src/components/Img.js
@@ -14,39 +14,41 @@ export default memo(function Img(props) {
       <Transition show={isOpen}>
         <Portal>
           <div className="fixed z-10 inset-0 overflow-y-auto" onClick={() => setIsOpen(false)}>
-            <div className='flex items-center justify-center min-h-screen text-center'>
+            <div className="flex items-center justify-center min-h-screen text-center">
               <Transition.Child
-                enter="transition ease-out duration-300"
-                enterFrom="opacity-0"
-                enterTo="opacity-100"
-                leave="transition ease-in duration-200"
-                leaveFrom="opacity-100"
-                leaveTo="opacity-0"
+                className="fixed inset-0 transition-opacity"
+                aria-hidden="true"
+                enter='ease-out duration-300'
+                enterFrom='opacity-0'
+                enterTo='opacity-100'
+                leave='ease-in duration-200'
+                leaveFrom='opacity-100'
+                leaveTo='opacity-0'
               >
-                <div className="fixed inset-0 transition-opacity" aria-hidden="true">
-                  <div className="absolute inset-0 bg-white dark:bg-gray-900 opacity-75" />
-                </div>
+                <div className="absolute inset-0 bg-white dark:bg-gray-900 opacity-75" />
               </Transition.Child>
               <span className="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">&#8203;</span>
               <Transition.Child
-                enter="transition ease-out duration-300"
-                enterFrom="opacity-0 scale-95"
-                enterTo="opacity-100 scale-100"
-                leave="transition ease-in duration-200"
-                leaveFrom="opacity-100 scale-100"
-                leaveTo="opacity-0 scale-95"
+                className="inline-block align-middle overflow-hidden shadow-xl transform transition-all"
+                role="dialog"
+                aria-modal="true"
+                aria-label="image zoom"
+                enter='ease-out duration-300'
+                enterFrom='opacity-0 scale-95'
+                enterTo='opacity-100 scale-100'
+                leave='ease-in duration-200'
+                leaveFrom='opacity-100 scale-100'
+                leaveTo='opacity-0 scale-95'
               >
-                <div className='inline-block align-middle overflow-hidden transform transition-all' role="dialog" aria-modal="true" aria-label="photo">
-                  <Img
-                    className="max-w-screen max-h-screen object-contain mx-auto cursor-zoom-out"
-                    src={props.src}
-                    alt={props.alt}
-                    width="460"
-                    height="460"
-                    decoding="async"
-                    loading="lazy"
-                  />
-                </div>
+                <img
+                  className="max-w-screen max-h-screen object-contain mx-auto cursor-zoom-out"
+                  src={props.src}
+                  alt={props.alt}
+                  width="460"
+                  height="460"
+                  decoding="async"
+                  loading="lazy"
+                />
               </Transition.Child>
             </div>
           </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -43,5 +43,6 @@ module.exports = {
     require('@tailwindcss/forms'),
     require('@tailwindcss/typography'),
     require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/line-clamp'),
   ],
 };
